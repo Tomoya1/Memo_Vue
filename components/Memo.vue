a0817d77e9ec210ec98d41809e8dfd1565ac3ef8
筒井智也 <tomoya.tmy@gmail.com>
2019-04-06 18:34:25 +0900

    firstcommit

---
+++ components/Memo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    :style="{
+      top: `${posY}px`,
+      left: `${posX}px`
+    }"
+    class="memo">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posX: {
+      type: Number,
+      required: true
+    },
+    posY: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+.memo {
+  position: fixed;
+  width: 200px;
+  height: 300px;
+  background: #ff0;
+}
+</style>
